20a9079850c8affe0de93727587643abfbb7c346
Emon Khalid <emonkhalid@gmail.com>
2018-10-18 17:48:24 +0600

    -A custom layout has been created. -Add java code inside main activity to display custom layout as Toast

--- app/src/main/java/com/example/user/inflatealayout/MainActivity.java
+++ app/src/main/java/com/example/user/inflatealayout/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.user.inflatealayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnloadlayout(View view) {
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View view1 = layoutInflater.inflate(R.layout.custom_layout,null);
+        TextView tv = view1.findViewById(R.id.textView);
+        tv.setText("Custom Layout Title");
+        Toast toast = new Toast(getApplicationContext());
+        toast.setView(view1);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL,0,0);
+        toast.show();
     }
 }
